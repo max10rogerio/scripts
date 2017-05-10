162e0fd967bdbf14669291a43682a7be51151da2
max10rogerio <max10rogerio@hotmail.com>
2017-05-10 17:08:24 -0300

    alterar codificação do banco

---
+++ alter-collation.php
@@ -0,0 +1,12 @@
+<?php
+
+  $con = mysqli_connect('localhost','root','', 'intranet.famma');
+  if(!$con) { echo "Cannot connect to the database ";die();}
+  $result=mysqli_query($con, 'show tables');
+  while($tables = mysqli_fetch_array($result)) {
+          foreach ($tables as $key => $value) {
+           mysqli_query($con, "ALTER TABLE $value CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci");
+     }}
+  echo "The collation of your database has been successfully changed!";
+
+?>
